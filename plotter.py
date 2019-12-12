d5c415b6306eeb2bfc50815f9edc3fc3edc256ad
Erik Vroon <erik.vroon22@gmail.com>
2019-12-12 12:30:32 +0100

    Fix errors in plotting

--- plotter.py
+++ plotter.py
@@ -1,12 +1,14 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import model
 from collections import OrderedDict
 
 plt.rcParams.update({'font.size': 16})
 
 eta, etd, flight_count, arrival_flights = model.eta, model.etd, model.flight_count, model.arrival_flights
+
 
 def set_legend():
     handles, labels = plt.gca().get_legend_handles_labels()
@@ -22,11 +24,9 @@ def set_legend():
 
 def setup_gantt_plot():
     fig, gnt = plt.subplots(figsize=(18, 16))
-    gnt.set_ylim(0, len(model.bays))
     gnt.set_xlim(0, 24)
 
     gnt.set_xlabel('Time (hours)')
-    gnt.set_ylabel('Bay')
 
     time_ticks = [str(x) for x in np.arange(0, 24)]
     gnt.set_xticks(np.arange(0, 24))
@@ -34,13 +34,15 @@ def setup_gantt_plot():
 
     gnt.yaxis.grid()
 
-    cmap = plt.cm.get_cmap('winter_r')    
+    cmap = plt.cm.get_cmap('winter_r')
     return gnt, cmap
 
 def plot_bay_assignment():
     gnt, cmap = setup_gantt_plot()
     assignments = np.genfromtxt(model.results_path + 'bay_assignment_result.csv', delimiter=';')
 
+    gnt.set_ylabel('Bay')
+    gnt.set_ylim(0, len(model.bays))
     gnt.set_yticks(np.arange(len(model.bays)) + 0.5)
     gnt.set_yticklabels(model.bays)
 
@@ -67,6 +69,8 @@ def plot_gate_assignment():
     gnt, cmap = setup_gantt_plot()
     assignments = np.genfromtxt(model.results_path + 'gate_assignment_result.csv', delimiter=';')
 
+    gnt.set_ylabel('Gate')
+    gnt.set_ylim(0, len(model.gates))
     gnt.set_yticks(np.arange(len(model.gates)) + 0.5)
     gnt.set_yticklabels(model.gates)
 
@@ -84,11 +88,9 @@ def plot_gate_assignment():
         departure = etd[i] - model.buffer / 60.0
         stay_period = departure - arrival
 
-        gnt.broken_barh([(arrival, stay_period)], (gate - 1, 1), facecolors=rgba, label=ac_class)
+        gnt.broken_barh([(arrival, stay_period)], (gate - 1, 1), facecolors=[0, 0, 1, 0.5], label=ac_class)
     
-    set_legend()
     plt.savefig(model.results_path + 'gate_assignment.png', bbox_inches='tight')
 
 plot_bay_assignment()
 plot_gate_assignment()
-plt.show()
