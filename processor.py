3221ec96ae172a70573f64e4a51d1a43fa7f1735
Erik Vroon <erik.vroon22@gmail.com>
2020-01-30 14:36:49 +0100

    Add preferred gates and bays to results

--- processor.py
+++ processor.py
@@ -41,9 +41,11 @@ def add_solutions_to_flight_data():
     data = open(model.data_path + "flights_processed.csv", "r").read().split('\n')
 
     for i, _ in enumerate(data[1:-1]):
-        data[i+1] += ';' + gate_assignments[i] + ';' + bays[int(bay_assignments[i])]
+        pref_gate = model.flight_has_gate_preference(gate_assignments[i]])
+        pref_bay = model.flight_has_bay_preference(bays[int(bay_assignments[i])])
+        data[i+1] += ';' + gate_assignments[i] + ';' + bays[int(bay_assignments[i])] + ';' + pref_gate + ';' + pref_bay
 
-    output = data[0] + ';Gate;Bay\n' + '\n'.join(data[1:-1])
+    output = data[0] + ';Gate;Bay;Pref. Gate; Pref. Bay\n' + '\n'.join(data[1:-1])
     open(model.results_path + 'result.csv', 'w').write(output)
     
 process_bay_assignment_cplex()
